b2d948ed174fd71e1e8c2fc3d2fbb2f942ab9a77
BL-Poojass <pooja.ss@bridgelabz.com>
2025-12-31 13:21:20 +0530

    UC4: Reusable method for banner

--- src/com/banner/app/UC2_OOPSBanner.java
+++ src/com/banner/app/UC2_OOPSBanner.java
@@ -3,9 +3,9 @@ package com.banner.app;
 public class UC2_OOPSBanner {
     public static void main(String[] args) {
         System.out.println(" ***   ***   *****   ***** ");
-        System.out.println("*     *   *  *       *     ");
-        System.out.println("*     *   *  *****   ***** ");
-        System.out.println("*     *   *  *           * ");
-        System.out.println(" ***   ***   *****   ***** ");
+        System.out.println("*   * *   *  *   *   *     ");
+        System.out.println("*   * *   *  *****   ***** ");
+        System.out.println("*   * *   *  *           * ");
+        System.out.println(" ***   ***   *       ***** ");
     }
 }
